a67c04ba2e1e2f151842d78faaa5ed4f563237e7
alexbonavila <alexbonavila44@gmail.com>
2024-07-02 17:57:05 +0200

    routes api createds

--- routes/api.php
+++ routes/api.php
@@ -12,4 +12,13 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
 
+// Application Routes
+Route::middleware('auth:api')->group(function () {
+    Route::apiResource('devices', DeviceController::class);
+    Route::apiResource('configurations', ConfigurationController::class);
+    Route::apiResource('projects', ProjectController::class);
+    Route::apiResource('reports', ReportController::class);
 
+    Route::post('projects/{project}/attach-user', [ProjectController::class, 'attachUser']);
+    Route::post('projects/{project}/detach-user', [ProjectController::class, 'detachUser']);
+});
