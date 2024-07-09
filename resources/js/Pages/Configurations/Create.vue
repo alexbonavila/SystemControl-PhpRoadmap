51c9917d17dd667f03a10289449b65ec13a749f7
alexbonavila <alexbonavila44@gmail.com>
2024-07-09 18:43:06 +0200

    Basic CRUD frontend in configurations

---
+++ resources/js/Pages/Configurations/Create.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Configuration from '@/Pages/Configurations/Configuration.vue';
+import AppLayout from '@/Layouts/AppLayout.vue';
+</script>
+
+<template>
+    <AppLayout title="Create Configuration">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                Create Configuration
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+                    <Configuration :configuration="{ id: null, device_id: '', cpu: '', ram: '', storage: '' }" @close="$inertia.visit(route('configurations.index'))" />
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
