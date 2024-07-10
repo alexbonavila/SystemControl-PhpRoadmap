247b25a894adbe45c883dfd85ebe6deb7c0704c4
alexbonavila <alexbonavila44@gmail.com>
2024-07-10 18:32:15 +0200

    Jobs scaffold created

---
+++ app/Jobs/SendBackupNotification.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SendBackupNotification implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $backupPath;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($backupPath)
+    {
+        $this->backupPath = $backupPath;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        // Mail to notify backup performed
+    }
+}
