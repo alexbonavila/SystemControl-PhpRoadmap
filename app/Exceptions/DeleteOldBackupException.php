3ffd4b6fb5fede4be99901ab5b01240bcd916b56
alexbonavila <alexbonavila44@gmail.com>
2024-07-13 15:39:15 +0200

    Implementing customized exceptions

--- app/Exceptions/DeleteOldBackupException.php
+++ app/Exceptions/DeleteOldBackupException.php
@@ -3,8 +3,17 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Support\Facades\Log;
 
 class DeleteOldBackupException extends Exception
 {
-    //
+    public function report()
+    {
+        Log::error('Error deleting old backups: ' . $this->getMessage());
+    }
+
+    public function render($request)
+    {
+        return response()->json(['error' => 'Error deleting old backups.'], 500);
+    }
 }
--- app/Exceptions/UploadNewBackupException.php
+++ app/Exceptions/UploadNewBackupException.php
@@ -3,8 +3,17 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Support\Facades\Log;
 
 class UploadNewBackupException extends Exception
 {
-    //
+    public function report()
+    {
+        Log::error('Error uploading backup: ' . $this->getMessage());
+    }
+
+    public function render($request)
+    {
+        return response()->json(['error' => 'Error uploading backup.'], 500);
+    }
 }
